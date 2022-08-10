312336e002d48951c8ecb4db143558e141c4852e
tgsmikema <sma148@aucklanduni.ac.nz>
2022-08-10 21:38:44 +0800

    add new/ `Laptop` class

---
+++ app/src/main/java/com/example/project_1_java_new_team42/Models/Laptop.java
@@ -0,0 +1,9 @@
+package com.example.project_1_java_new_team42.Models;
+
+public class Laptop extends Item{
+
+    public Laptop(String id, String name, String imagePath, String category, int price, String description) {
+        super(id, name, imagePath, category, price, description);
+    }
+    
+}
