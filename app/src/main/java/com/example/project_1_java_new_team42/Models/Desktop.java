729299fe07a09e01be0e049a9fbc8d882a07d75b
tgsmikema <sma148@aucklanduni.ac.nz>
2022-08-10 21:39:50 +0800

    add new/ `Desktop` class

---
+++ app/src/main/java/com/example/project_1_java_new_team42/Models/Desktop.java
@@ -0,0 +1,9 @@
+package com.example.project_1_java_new_team42.Models;
+
+public class Desktop extends Item{
+
+    public Desktop(String id, String name, String imagePath, String category, int price, String description) {
+        super(id, name, imagePath, category, price, description);
+    }
+
+}
