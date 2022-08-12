7039e55c1e2d661a571db84ccb53ac888afa625f
tgsmikema <sma148@aucklanduni.ac.nz>
2022-08-12 11:49:50 +0800

    added logic for ItemsDataFetcher Class

--- app/src/main/java/com/example/project_1_java_new_team42/Data/ItemsDataFetcher.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Data/ItemsDataFetcher.java
@@ -1,25 +1,56 @@
 package com.example.project_1_java_new_team42.Data;
 
-import com.example.project_1_java_new_team42.Models;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.project_1_java_new_team42.Models.Desktop;
+import com.example.project_1_java_new_team42.Models.IItem;
+import com.example.project_1_java_new_team42.Models.Laptop;
+import com.example.project_1_java_new_team42.Models.Tablet;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemsDataFetcher {
-    IItems fetchItems(IFetchHandler fetchHandler) {
-        List<IItems> items = new ArrayList<IItems>(); // Use any list implementation as long consistent
+    void fetchItems(IFetchHandler fetchHandler) {
+        List<IItem> itemsList = new ArrayList<IItem>(); // Use any list implementation as long consistent
 
-        firestore...addOnCompleteListener(new onCompleteListener()) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("items").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            onComplete() {
-                if (task.isSuccessful) {
-                    QuerySnapshot results = task.getResult();
-                    for (IItems item : task.getResult().toObjects(Number.class)) {
-                        items.add(aNumber);
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot queryItem : task.getResult()) {
+                        IItem anItem;
+                        if (queryItem.get("category").toString().equalsIgnoreCase("laptop")) {
+                            anItem = queryItem.toObject(Laptop.class);
+                            itemsList.add(anItem);
+                        } else if (queryItem.get("category").toString().equalsIgnoreCase("tablet")) {
+                            anItem = queryItem.toObject(Tablet.class);
+                            itemsList.add(anItem);
+                        } else if (queryItem.get("category").toString().equalsIgnoreCase("desktop")) {
+                            anItem = queryItem.toObject(Desktop.class);
+                            itemsList.add(anItem);
+                        } else {
+                            throw new UnsupportedOperationException("Class Unimplemented ERROR!");
+                        }
+                        fetchHandler.onFetchComplete(itemsList);
+                        Log.i("Parsing Items", anItem.getId() + " loaded.");
                     }
-                    fetchHandler.onFetchComplete(items)
-                    return items
+
                 } else {
-                    fetchHandler.onFetchFail()
+                    fetchHandler.onFetchFail();
                 }
             }
-        })
+        });
     }
 }
+
