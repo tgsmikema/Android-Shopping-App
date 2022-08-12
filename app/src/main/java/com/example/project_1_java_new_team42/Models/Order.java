6393da087d3c5b7b711d13986dc5791f1ae0f555
tgsmikema <sma148@aucklanduni.ac.nz>
2022-08-12 22:55:21 +0800

    update-Models / Order class fields

--- app/src/main/java/com/example/project_1_java_new_team42/Models/Order.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Models/Order.java
@@ -1,13 +1,30 @@
 package com.example.project_1_java_new_team42.Models;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Order {
 
+    private int orderId;
+    private LocalDateTime placedDateAndTime;
+    private int totalCost;
     private List<ItemWithQuantity> items;
 
+    //empty constructor
     public Order(){
 
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public LocalDateTime getPlacedDateAndTime() {
+        return placedDateAndTime;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
     }
 
     public List<ItemWithQuantity> getOrderItems(){
