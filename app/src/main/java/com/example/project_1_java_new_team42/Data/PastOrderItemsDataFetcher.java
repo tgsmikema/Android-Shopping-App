b7837a9e2a43cf9bac7ed0d1e4cebcfbfe0d2e58
tgsmikema <sma148@aucklanduni.ac.nz>
2022-08-13 04:26:59 +0800

    feat-database / implemented Past order ITEMS data fetcher logic

---
+++ app/src/main/java/com/example/project_1_java_new_team42/Data/PastOrderItemsDataFetcher.java
@@ -0,0 +1,38 @@
+package com.example.project_1_java_new_team42.Data;
+
+import androidx.annotation.NonNull;
+
+import com.example.project_1_java_new_team42.Models.Order;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PastOrderItemsDataFetcher {
+
+    public void readData(String orderId, IPastOrderItemsDataFetchHandler dataFetchHandler) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        List<Order> ordersList = new ArrayList<>();
+
+        db.collection("orders").whereEqualTo("orderId",orderId).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot queryItem : task.getResult()) {
+
+                        Order anOrder = queryItem.toObject(Order.class);
+                        ordersList.add(anOrder);
+                    }
+                    dataFetchHandler.onFetchComplete(ordersList);
+                } else {
+                    dataFetchHandler.onFetchFail();
+                }
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/project_1_java_new_team42/MainActivity.java
+++ app/src/main/java/com/example/project_1_java_new_team42/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.project_1_java_new_team42.Data.IPastOrderItemsDataFetchHandler;
 import com.example.project_1_java_new_team42.Data.IPastOrdersDataFetchHandler;
+import com.example.project_1_java_new_team42.Data.PastOrderItemsDataFetcher;
 import com.example.project_1_java_new_team42.Data.PastOrdersDataFetcher;
 import com.example.project_1_java_new_team42.Models.Order;
 
@@ -12,21 +14,21 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    PastOrdersDataFetcher pastO = new PastOrdersDataFetcher();
+    PastOrderItemsDataFetcher pastO = new PastOrderItemsDataFetcher();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pastO.readData(new IPastOrdersDataFetchHandler() {
+        pastO.readData("20220813041837684",new IPastOrderItemsDataFetchHandler() {
             @Override
             public void onFetchComplete(List<Order> ordersList) {
                 System.out.println("------------------------------------------------");
                 System.out.println(ordersList.size());
                 System.out.println(ordersList.get(0).getPlacedDateAndTime());
                 System.out.println(ordersList.get(0).getOrderItems());
-                System.out.println(ordersList.get(0).getOrderItems().get(0));
+                System.out.println(ordersList.get(0).getOrderItems().get(0).getDescription());
             }
 
             @Override
