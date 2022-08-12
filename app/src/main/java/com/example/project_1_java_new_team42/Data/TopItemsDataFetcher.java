6d659a28f6a053374c9add341ab1acc9a530e214
tgsmikema <sma148@aucklanduni.ac.nz>
2022-08-12 21:46:52 +0800

    feat - database / implemented TopItemsData fetching logic

--- app/src/main/java/com/example/project_1_java_new_team42/Data/TopItemsDataFetcher.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Data/TopItemsDataFetcher.java
@@ -11,17 +11,22 @@ import com.example.project_1_java_new_team42.Models.Tablet;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.core.OrderBy;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TopItemsDataFetcher extends AssignCategory {
+
+    final private int NO_OF_TOP_ITEMS_TO_FETCH = 6;
+
     public void readData(ITopItemsDataFetchHandler dataFetchHandler) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        db.collection("items").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        db.collection("items").orderBy("totalSold", Query.Direction.DESCENDING).limit(NO_OF_TOP_ITEMS_TO_FETCH).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
--- app/src/main/java/com/example/project_1_java_new_team42/MainActivity.java
+++ app/src/main/java/com/example/project_1_java_new_team42/MainActivity.java
@@ -6,33 +6,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.project_1_java_new_team42.Data.IItemDetailsDataFetchHandler;
 import com.example.project_1_java_new_team42.Data.ISearchItemsDataFetchHandler;
+import com.example.project_1_java_new_team42.Data.ITopItemsDataFetchHandler;
 import com.example.project_1_java_new_team42.Data.ItemDetailsDataFetcher;
 import com.example.project_1_java_new_team42.Data.SearchItemsDataFetcher;
+import com.example.project_1_java_new_team42.Data.TopItemsDataFetcher;
 import com.example.project_1_java_new_team42.Models.IItem;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    SearchItemsDataFetcher fetch =  new SearchItemsDataFetcher();
+    TopItemsDataFetcher fetch =  new TopItemsDataFetcher();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fetch.readData(new ISearchItemsDataFetchHandler() {
+        fetch.readData(new ITopItemsDataFetchHandler() {
             @Override
             public void onFetchComplete(List<IItem> itemsList) {
                 System.out.println("----------------------------------------------------");
-                System.out.println(itemsList.size());
-                System.out.println(itemsList.get(0).getName());
+                for (IItem anItem : itemsList){
+                    System.out.println(anItem.getName() + "sold: " + anItem.getTotalSold());
+                }
             }
 
             @Override
             public void onFetchFail() {
 
             }
-        }, "2022");
+        });
     }
 }
