89438bf88f52c2a9e4a1c265bf634d85fcaca812
Andy Fong <afon072@aucklanduni.ac.nz>
2022-08-12 15:29:52 +1200

    Added initial code to ItemsDataFetecher class

--- app/src/main/java/com/example/project_1_java_new_team42/Data/ItemsDataFetcher.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Data/ItemsDataFetcher.java
@@ -1,5 +1,25 @@
 package com.example.project_1_java_new_team42.Data;
 
-public class ItemsDataFetcher {
+import com.example.project_1_java_new_team42.Models;
 
+public class ItemsDataFetcher {
+    IItems fetchItems(IFetchHandler fetchHandler) {
+        List<IItems> items = new ArrayList<IItems>(); // Use any list implementation as long consistent
+
+        firestore...addOnCompleteListener(new onCompleteListener()) {
+            @Override
+            onComplete() {
+                if (task.isSuccessful) {
+                    QuerySnapshot results = task.getResult();
+                    for (IItems item : task.getResult().toObjects(Number.class)) {
+                        items.add(aNumber);
+                    }
+                    fetchHandler.onFetchComplete(items)
+                    return items
+                } else {
+                    fetchHandler.onFetchFail()
+                }
+            }
+        })
+    }
 }
