b708948ddb99cdb0fcb38cf3a18645b75ee74870
Andy Fong <afon072@aucklanduni.ac.nz>
2022-08-16 15:04:24 +1200

    Created CartActivity class and activity_cart layout resource file

---
+++ app/src/main/java/com/example/project_1_java_new_team42/Activities/CartActivity.java
@@ -0,0 +1,16 @@
+package com.example.project_1_java_new_team42.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project_1_java_new_team42.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
