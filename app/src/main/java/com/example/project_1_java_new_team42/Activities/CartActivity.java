3e86f5d056498a56412bf55473aef24c8c1d7ba6
Andy Fong <afon072@aucklanduni.ac.nz>
2022-08-16 17:22:27 +1200

    Added RecyclerView to activity_cart.xml file and added basic code to initialize cartRecyclerView in CartActivity class

--- app/src/main/java/com/example/project_1_java_new_team42/Activities/CartActivity.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Activities/CartActivity.java
@@ -3,10 +3,29 @@ package com.example.project_1_java_new_team42.Activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.project_1_java_new_team42.Adapters.ItemsRecyclerViewAdapter;
+import com.example.project_1_java_new_team42.Data.Fetchers.CartDataFetcher;
 import com.example.project_1_java_new_team42.R;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 
 public class CartActivity extends AppCompatActivity {
+
+    protected RecyclerView cartRecyclerView;
+//    protected ItemsRecyclerViewAdapter topItemsAdapter;
+//    protected CircularProgressIndicator topItemsSpinner;
+    protected CartDataFetcher cartDataFetcher = new CartDataFetcher();
+
+    protected void initializeCartRecyclerView() {
+        cartRecyclerView = findViewById(R.id.recycler_view_cart);
+        cartRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+//        cartAdapter = new ItemsRecyclerViewAdapter(this);
+
+//        cartRecyclerView.setAdapter(cartAdapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
