11181ff50331e331a6869ee5c91204e3e8f20d8b
Andy Fong <afon072@aucklanduni.ac.nz>
2022-08-19 04:53:49 +1200

    Added divider to activity_cart.xml and started designing cart item card

--- app/src/main/java/com/example/project_1_java_new_team42/Activities/CartActivity.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Activities/CartActivity.java
@@ -31,5 +31,7 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        initializeCartRecyclerView();
     }
 }
