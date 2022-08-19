67b3a03426730a30b74feb7ce7d663f78bf53e41
tgsmikema <sma148@aucklanduni.ac.nz>
2022-08-19 21:28:59 +0800

    refactor DetailsActivity

--- app/src/main/java/com/example/project_1_java_new_team42/Activities/DetailsActivity.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Activities/DetailsActivity.java
@@ -2,17 +2,15 @@ package com.example.project_1_java_new_team42.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.example.project_1_java_new_team42.Adapters.CategoriesRecyclerViewAdapter;
 import com.example.project_1_java_new_team42.Adapters.ImageSliderAdapter;
 import com.example.project_1_java_new_team42.Data.Fetchers.IFetchHandler;
 import com.example.project_1_java_new_team42.Data.Fetchers.ItemDetailsDataFetcher;
@@ -20,70 +18,42 @@ import com.example.project_1_java_new_team42.Models.IItem;
 import com.example.project_1_java_new_team42.R;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    private class ViewHolder {
+
+        ViewPager imageSlider;
+        CircularProgressIndicator imageSpinner;
+        LinearLayout imageSliderDotPanel;
+
+        public ViewHolder() {
+            imageSlider = findViewById(R.id.image_slide_details);
+            imageSpinner = findViewById(R.id.progress_images);
+            imageSliderDotPanel = findViewById(R.id.image_slider_dots);
+        }
+    }
+
+    ViewHolder vh;
+
     protected ImageSliderAdapter imageSliderAdapter;
-    protected ViewPager imageSlider;
-    protected CircularProgressIndicator imageSpinner;
     protected ItemDetailsDataFetcher itemDetailsDataFetcher = new ItemDetailsDataFetcher();
 
-    LinearLayout sliderDotspanel;
-    private int dotscount;
+    private int dotsCount;
     private ImageView[] dots;
 
     private class ItemDetailsFetchHandler implements IFetchHandler<List<IItem>> {
 
             @Override
             public void onFetchComplete(List<IItem> data) {
-
+                // pass data from DB to image slider adapter to populate.
                 imageSliderAdapter.setData(data);
-                imageSlider.setAdapter(imageSliderAdapter);
-
-                imageSpinner.setVisibility(View.GONE);
-
-                dotscount = imageSliderAdapter.getCount();
-                dots = new ImageView[dotscount];
-
-                for(int i = 0; i < dotscount; i++){
-
-                    dots[i] = new ImageView(DetailsActivity.this);
-                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.non_active_dot));
-
-                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
-                    params.setMargins(8, 0, 8, 0);
-
-                    sliderDotspanel.addView(dots[i], params);
-
-                }
-
-                dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
-
-                imageSlider.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-                    @Override
-                    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-                    }
-
-                    @Override
-                    public void onPageSelected(int position) {
-
-                        for(int i = 0; i< dotscount; i++){
-                            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.non_active_dot));
-                        }
-
-                        dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
-
-                    }
-
-                    @Override
-                    public void onPageScrollStateChanged(int state) {
-
-                    }
-                });
+                vh.imageSlider.setAdapter(imageSliderAdapter);
+                // remove the display of the waiting icon from the screen
+                vh.imageSpinner.setVisibility(View.GONE);
+                // display the image slider dots
+                initializeImageSliderDots();
             }
 
             @Override
@@ -97,26 +67,51 @@ public class DetailsActivity extends AppCompatActivity {
             imageSliderAdapter = new ImageSliderAdapter(this);
         }
 
+        protected void initializeImageSliderDots(){
+            dotsCount = imageSliderAdapter.getCount();
+            dots = new ImageView[dotsCount];
+            for(int i = 0; i < dotsCount; i++){
+                dots[i] = new ImageView(DetailsActivity.this);
+                dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.non_active_dot));
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                layoutParams.setMargins(8, 0, 8, 0);
+                vh.imageSliderDotPanel.addView(dots[i], layoutParams);
+            }
+            dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+            vh.imageSlider.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+                @Override
+                public void onPageSelected(int position) {
+                    for(int i = 0; i< dotsCount; i++){
+                        dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.non_active_dot));
+                    }
+                    dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+                }
+                @Override
+                public void onPageScrollStateChanged(int state) {}
+            });
+        }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        imageSlider = findViewById(R.id.image_slide_details);
-        imageSpinner = findViewById(R.id.progress_images);
+        vh = new ViewHolder();
 
         initializeImageSlider();
+        // ----------TESTING PURPOSE, REPLACE Once Developed Navigation-------(API)--------
+        // Intent intent = getIntent();
+        // String str = intent.getStringExtra("message_key");
+        itemDetailsDataFetcher.readData("laptop_1", new ItemDetailsFetchHandler());
+        // pass in id(string) of the selected Item.
+        // SENDER SIDE (API) CODE
+        // Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
+        // intent.putExtra("message_key", "string");
+        // startActivity(intent);
+        // -----------------------------TESTING ENDS----------------------------------------
 
-        //---------------------------------------
-        sliderDotspanel = (LinearLayout) findViewById(R.id.image_slider_dots);
 
-
-        //------------------------------------------
-
-
-
-
-
-        itemDetailsDataFetcher.readData("tablet_1", new ItemDetailsFetchHandler());
     }
 }
