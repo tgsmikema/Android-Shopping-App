832676a695b29963464da198b0327005122945a5
Andy Fong <afon072@aucklanduni.ac.nz>
2022-08-19 23:59:57 +1200

    Added a spinner to the cart activity. Created CartRecyclerViewAdapter (yet to be implemented)

--- app/src/main/java/com/example/project_1_java_new_team42/Activities/CartActivity.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Activities/CartActivity.java
@@ -1,22 +1,55 @@
 package com.example.project_1_java_new_team42.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.project_1_java_new_team42.Adapters.ItemsRecyclerViewAdapter;
+import com.example.project_1_java_new_team42.Adapters.CartRecyclerViewAdapter;
 import com.example.project_1_java_new_team42.Data.Fetchers.CartDataFetcher;
+import com.example.project_1_java_new_team42.Data.Fetchers.IFetchHandler;
+import com.example.project_1_java_new_team42.Models.Cart;
+import com.example.project_1_java_new_team42.Models.IItem;
 import com.example.project_1_java_new_team42.R;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 
+import java.util.List;
+
 public class CartActivity extends AppCompatActivity {
+    private static final String TAG = "CartActivity";
 
     protected RecyclerView cartRecyclerView;
-//    protected ItemsRecyclerViewAdapter topItemsAdapter;
-//    protected CircularProgressIndicator topItemsSpinner;
+    protected CartRecyclerViewAdapter cartItemsAdapter;
+    protected CircularProgressIndicator cartItemsSpinner;
     protected CartDataFetcher cartDataFetcher = new CartDataFetcher();
+
+    Button increment, decrement;
+    TextView quantity;
+
+    private class CartFetchHandler implements IFetchHandler<Cart> {
+        @Override
+        public void onFetchComplete(Cart data) {
+//            cartItemsAdapter.setData(data);
+//            cartItemsAdapter.notifyItemRangeInserted(0, data.size());
+
+            cartItemsSpinner.setVisibility(View.GONE);
+
+            Log.i(TAG, "Fetched items successfully");
+        }
+
+        @Override
+        public void onFetchFail() {
+            System.out.println("Failed to fetch items");
+            Toast.makeText(getApplicationContext(), "Failed to fetch categories", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     protected void initializeCartRecyclerView() {
         cartRecyclerView = findViewById(R.id.recycler_view_cart);
@@ -32,6 +65,14 @@ public class CartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
+        cartItemsSpinner = findViewById(R.id.progress_cart_items);
+
         initializeCartRecyclerView();
+
+        cartDataFetcher.readData(new CartFetchHandler());
+
+        increment = findViewById(R.id.button_plus);
+        decrement = findViewById(R.id.button_minus);
+        quantity = findViewById(R.id.item_quantity);
     }
 }
---
+++ app/src/main/java/com/example/project_1_java_new_team42/Adapters/CartRecyclerViewAdapter.java
@@ -0,0 +1,4 @@
+package com.example.project_1_java_new_team42.Adapters;
+
+public class CartRecyclerViewAdapter {
+}
