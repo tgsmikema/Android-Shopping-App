bd1bc7f1321f3736d2e1d0f202a6d75a5d90fafa
Dean Phommahaxay <deanp21@gmail.com>
2022-08-21 17:17:24 +1200

    Fetch and display category items with loading spinner

--- app/src/main/java/com/example/project_1_java_new_team42/Activities/CategoryItemsActivity.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Activities/CategoryItemsActivity.java
@@ -2,18 +2,48 @@ package com.example.project_1_java_new_team42.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.project_1_java_new_team42.Adapters.ItemsRecyclerViewAdapter;
+import com.example.project_1_java_new_team42.Data.Fetchers.CategoryItemsDataFetcher;
+import com.example.project_1_java_new_team42.Data.Fetchers.IFetchHandler;
 import com.example.project_1_java_new_team42.Models.Category;
+import com.example.project_1_java_new_team42.Models.IItem;
 import com.example.project_1_java_new_team42.R;
+import com.example.project_1_java_new_team42.Widgets.ItemsRecyclerView;
+import com.example.project_1_java_new_team42.Widgets.RecyclerViewLayoutType;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+
+import java.util.List;
 
 public class CategoryItemsActivity extends AppCompatActivity {
     private static final String TAG = "CategoryItemsActivity";
 
-    private Category category;
+    private final CategoryItemsDataFetcher itemsFetcher = new CategoryItemsDataFetcher();
+    private ItemsRecyclerViewAdapter itemsAdapter;
+    private CircularProgressIndicator spinner;
+
+    private class CategoryItemsFetchHandler implements IFetchHandler<List<IItem>> {
+        @Override
+        public void onFetchComplete(List<IItem> data) {
+           itemsAdapter.setData(data);
+           itemsAdapter.notifyItemRangeInserted(0, data.size());
+           spinner.setVisibility(View.GONE);
+           Log.i(TAG, "Fetched category  successfully");
+        }
+
+        @Override
+        public void onFetchFail() {
+            System.out.println("Failed to fetch items");
+            Toast.makeText(getApplicationContext(), "Failed to fetch items", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     private Category constructCategoryFromIntent() {
         Intent intent = getIntent();
@@ -33,14 +63,23 @@ public class CategoryItemsActivity extends AppCompatActivity {
         headingImageView.setImageResource(drawableId);
     }
 
+    private void initializeItemsRecyclerView() {
+        ItemsRecyclerView searchedItemsRV = new ItemsRecyclerView(this, findViewById(R.id.recycler_view_category_items), RecyclerViewLayoutType.GRID);
+        itemsAdapter = searchedItemsRV.getAdapter();
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_items);
 
-        category = constructCategoryFromIntent();
+        Category category = constructCategoryFromIntent();
         setCategoryViews(category);
 
-        // TODO Fetch using the doc id
-        // String docId = category.getDocId();
+        spinner = findViewById(R.id.progress_category_items);
+
+        initializeItemsRecyclerView();
+
+        String docId = category.getDocId();
+        itemsFetcher.readData(docId, new CategoryItemsFetchHandler());
     }
 }
