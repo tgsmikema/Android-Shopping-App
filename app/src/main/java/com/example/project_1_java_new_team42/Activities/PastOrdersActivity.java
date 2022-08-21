17f76f2dd69378c6edd76515c114999659fd2bd8
tgsmikema <sma148@aucklanduni.ac.nz>
2022-08-21 19:57:56 +0800

    designed UI for Past Orders Activity

---
+++ app/src/main/java/com/example/project_1_java_new_team42/Activities/PastOrdersActivity.java
@@ -0,0 +1,16 @@
+package com.example.project_1_java_new_team42.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project_1_java_new_team42.R;
+
+public class PastOrdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_orders);
+    }
+}
