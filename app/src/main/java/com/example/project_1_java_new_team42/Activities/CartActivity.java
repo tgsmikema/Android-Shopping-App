6b3e1050b68367f7ab9177b78a37b0f8483766ff
Andy Fong <afon072@aucklanduni.ac.nz>
2022-08-21 18:21:47 +1200

    Total Price TextView now updates from database

--- app/src/main/java/com/example/project_1_java_new_team42/Activities/CartActivity.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Activities/CartActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,12 +18,14 @@ import com.example.project_1_java_new_team42.Data.Fetchers.CartDataFetcher;
 import com.example.project_1_java_new_team42.Data.Fetchers.IFetchHandler;
 import com.example.project_1_java_new_team42.Models.Cart;
 import com.example.project_1_java_new_team42.Models.IItem;
+import com.example.project_1_java_new_team42.Models.ItemWithQuantity;
 import com.example.project_1_java_new_team42.R;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CartActivity extends AppCompatActivity {
+public class CartActivity extends AppCompatActivity implements CartRecyclerViewAdapter.ButtonClickListener {
     private static final String TAG = "CartActivity";
 
     protected RecyclerView cartRecyclerView;
@@ -30,10 +33,7 @@ public class CartActivity extends AppCompatActivity {
     protected CircularProgressIndicator cartItemsSpinner;
     protected CartDataFetcher cartDataFetcher = new CartDataFetcher();
 
-    Button increment, decrement;
-    TextView quantity;
-
-    int count;
+    TextView totalPriceTextView;
 
     private class CartFetchHandler implements IFetchHandler<Cart> {
         @Override
@@ -44,6 +44,11 @@ public class CartActivity extends AppCompatActivity {
             cartItemsSpinner.setVisibility(View.GONE);
 
             Log.i(TAG, "Fetched cart items successfully");
+
+            totalPriceTextView = findViewById(R.id.cart_total_price);
+            String totalPrice = "$" + data.getTotalPrice();
+            totalPriceTextView.setText(totalPrice);
+            System.out.println(totalPrice);
         }
 
         @Override
@@ -63,6 +68,7 @@ public class CartActivity extends AppCompatActivity {
         cartRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
         cartItemsAdapter = new CartRecyclerViewAdapter(this);
+        cartItemsAdapter.setClickListener(this);
 
         cartRecyclerView.setAdapter(cartItemsAdapter);
     }
@@ -75,19 +81,30 @@ public class CartActivity extends AppCompatActivity {
         cartItemsSpinner = findViewById(R.id.progress_cart_items);
 
         initializeCartRecyclerView();
-
         cartDataFetcher.readData(new CartFetchHandler());
 
-        increment = findViewById(R.id.button_plus);
-        decrement = findViewById(R.id.button_minus);
-        quantity = findViewById(R.id.text_item_quantity);
+//        increment = findViewById(R.id.button_plus);
+//        decrement = findViewById(R.id.button_minus);
+//        quantity = findViewById(R.id.text_item_quantity);
 
 //        increment.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-//                count++;
-//                display.setText
+//                System.out.println("Increment working");
+//                System.out.println(itemsWithQuantity.get(0).getQuantity());
 //            }
 //        });
+//
+//        decrement.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                System.out.println("Decrement working");
+//
+//            }
+//        });
+    }
+
+    public void onButtonClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + cartItemsAdapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/project_1_java_new_team42/Adapters/CartRecyclerViewAdapter.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Adapters/CartRecyclerViewAdapter.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,6 +26,9 @@ public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerVi
     private LayoutInflater layoutInflater;
     private Context context;
 
+    private ButtonClickListener mClickListener;
+//    private OnDataChangeListener mOnDataChangeListener;
+
     public CartRecyclerViewAdapter(Context context) {
         this.context = context;
         this.layoutInflater = LayoutInflater.from(context);
@@ -39,6 +44,9 @@ public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerVi
         TextView cartItemNameTextView;
         TextView cartItemPriceTextView;
         TextView cartItemQuantityTextView;
+        Button increment;
+        Button decrement;
+        TextView quantity;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -47,14 +55,18 @@ public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerVi
             cartItemNameTextView = itemView.findViewById(R.id.text_cart_itemcard_name);
             cartItemPriceTextView = itemView.findViewById(R.id.text_cart_itemcard_price);
             cartItemQuantityTextView = itemView.findViewById(R.id.text_item_quantity);
+            increment = itemView.findViewById(R.id.button_plus);
+            decrement = itemView.findViewById(R.id.button_minus);
+            quantity = itemView.findViewById(R.id.text_item_quantity);
             itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
             // TODO Navigate to the item details page
-//            Cart item = cartItem.get(getAdapterPosition());
+//            ItemWithQuantity item = itemsWithQuantity.get(getAdapterPosition());
 //            Toast.makeText(context, "Name: " + item.getName() + " Price: " + item.getPrice(), Toast.LENGTH_SHORT).show();
+            if (mClickListener != null) mClickListener.onButtonClick(view, getAdapterPosition());
         }
     }
 
@@ -71,12 +83,37 @@ public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerVi
         int drawableId = context.getResources().getIdentifier(item.getImagePaths().get(0),"drawable", context.getPackageName());
         holder.cartItemImageView.setImageResource(drawableId);
         holder.cartItemNameTextView.setText(item.getName());
-        String price = "$" + cartData.getTotalPrice();
+        String price = "$" + item.getPrice() * item.getQuantity();
         holder.cartItemPriceTextView.setText(price);
         String quantity = "" + item.getQuantity();
         holder.cartItemQuantityTextView.setText(quantity);
+        System.out.println("total price1 = " + cartData.getTotalPrice());
     }
 
     @Override
     public int getItemCount() { return itemsWithQuantity.size(); }
+
+    public String getItem(int id) {
+        return itemsWithQuantity.get(id).getName();
+    }
+
+    public void setClickListener(ButtonClickListener buttonClickListener) {
+        this.mClickListener = buttonClickListener;
+    }
+
+    public interface ButtonClickListener {
+        void onButtonClick(View view, int position);
+    }
+
+//    private void doButton
+//
+//    public void setOnDataChangeListener(OnDataChangeListener onDataChangeListener) {
+//        mOnDataChangeListener = onDataChangeListener;
+//    }
+//
+//    public interface OnDataChangeListener {
+//        public void OnDataChanged(int size);
+//    }
+
+
 }
