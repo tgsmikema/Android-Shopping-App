6a8d834317fa1417fce528a93c2c92d60c190734
tgsmikema <sma148@aucklanduni.ac.nz>
2022-08-22 14:26:16 +0800

    implemented PastOrderItemsRecyclerViewAdapter class + initialize part of the PastOrderItemsActivity class parameters

--- app/src/main/java/com/example/project_1_java_new_team42/Activities/PastOrderItemsActivity.java
+++ app/src/main/java/com/example/project_1_java_new_team42/Activities/PastOrderItemsActivity.java
@@ -1,4 +1,76 @@
 package com.example.project_1_java_new_team42.Activities;
 
-public class PastOrderItemsActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project_1_java_new_team42.Adapters.PastOrdersRecyclerViewAdapter;
+import com.example.project_1_java_new_team42.Data.Fetchers.IFetchHandler;
+import com.example.project_1_java_new_team42.Data.Fetchers.PastOrderItemsDataFetcher;
+import com.example.project_1_java_new_team42.Models.Order;
+import com.example.project_1_java_new_team42.R;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.List;
+
+public class PastOrderItemsActivity extends AppCompatActivity {
+
+    private class ViewHolder {
+
+        Button backButton;
+
+        MaterialTextView orderNumber;
+        RecyclerView pastOrderItemsRecyclerView;
+        CircularProgressIndicator pastOrderItemsSpinner;
+        TextView orderDate, orderTotalPrice;
+
+        //Bottom Nav Bar
+        NavigationBarView bottomNavBar;
+
+        public ViewHolder() {
+            backButton = findViewById(R.id.button_back_past_order_items);
+
+            orderNumber = findViewById(R.id.order_number_past_order_items);
+            pastOrderItemsRecyclerView = findViewById(R.id.recycler_view_past_order_items);
+            pastOrderItemsSpinner = findViewById(R.id.progress_past_order_items);
+            orderDate = findViewById(R.id.order_date_past_order_items);
+            orderTotalPrice = findViewById(R.id.order_total_price_past_order_items);
+
+            // Bottom Nav Bar
+            bottomNavBar = findViewById(R.id.bottom_navigation_past_order_items);
+        }
+    }
+
+    ViewHolder vh;
+
+    protected PastOrdersRecyclerViewAdapter pastOrdersRecyclerViewAdapter;
+    // initialise the DB fetcher class
+    protected PastOrderItemsDataFetcher pastOrderItemsDataFetcher = new PastOrderItemsDataFetcher();
+
+    private class pastOrderItemsFetchHandler implements IFetchHandler<List<Order>> {
+
+        @Override
+        public void onFetchComplete(List<Order> data) {
+
+        }
+
+        @Override
+        public void onFetchFail() {
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_orders);
+
+        vh = new ViewHolder();
+    }
+
 }
---
+++ app/src/main/java/com/example/project_1_java_new_team42/Adapters/PastOrderItemsRecyclerViewAdapter.java
@@ -0,0 +1,77 @@
+package com.example.project_1_java_new_team42.Adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project_1_java_new_team42.Models.ItemWithQuantity;
+import com.example.project_1_java_new_team42.Models.Order;
+import com.example.project_1_java_new_team42.R;
+import com.google.android.material.card.MaterialCardView;
+
+// ItemWithQuantity Instead of Order Class
+public class PastOrderItemsRecyclerViewAdapter extends GenericRecyclerViewAdapter<ItemWithQuantity, PastOrderItemsRecyclerViewAdapter.ViewHolder> {
+
+    protected PastOrderItemsRecyclerViewAdapter(Context context) {
+        super(context);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        MaterialCardView orderItemCardView;
+        ImageView orderItemImage;
+        TextView orderItemName, orderItemQuantity, orderItemTotalPrice;
+
+        public ViewHolder(View orderView) {
+            super(orderView);
+            orderItemCardView = orderView.findViewById(R.id.material_past_order_item_card);
+
+            orderItemImage = orderView.findViewById(R.id.image_past_order_item_card);
+
+            orderItemName = orderView.findViewById(R.id.name_past_order_item_card);
+            orderItemQuantity = orderView.findViewById(R.id.quantity_past_order_item_card);
+            orderItemTotalPrice = orderView.findViewById(R.id.price_past_order_item_card);
+
+            orderItemCardView.setClickable(true);
+            orderItemCardView.setFocusable(true);
+            orderItemCardView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            //TODO implement onClick methods here.
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(layoutInflater.inflate(R.layout.past_order_item_card, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PastOrderItemsRecyclerViewAdapter.ViewHolder holder, int position) {
+        ItemWithQuantity itemWithQuantity = items.get(position);
+
+        int drawableId = context.getResources().getIdentifier(itemWithQuantity.getImagePaths().get(0),"drawable", context.getPackageName());
+        holder.orderItemImage.setImageResource(drawableId);
+
+        String itemName = itemWithQuantity.getName();
+        holder.orderItemName.setText(itemName);
+
+        String quantity = "Qty: " + itemWithQuantity.getQuantity();
+        holder.orderItemQuantity.setText(quantity);
+
+        String totalPrice = "$" + String.valueOf(itemWithQuantity.getQuantity() * itemWithQuantity.getPrice());
+        holder.orderItemTotalPrice.setText(totalPrice);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+}
